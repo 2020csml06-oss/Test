45d881b351c1bcec2f7d08363ff986c3f4c3660f
P Pavan Kalyan <ppavankalyan79@gmail.com>
2026-04-19 15:51:15 +0530

    test auto build

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.end('Hello from Jenkins Auto Deploy 🚀');
+    res.end('Auto build working 🔥');
 });
 
 server.listen(3000, () => {
