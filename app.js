d2b883c98f922bac8286e311214f6b6d653c312c
P Pavan Kalyan <ppavankalyan79@gmail.com>
2026-04-19 16:49:25 +0530

    Update new version 6

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const server = http.createServer((req, res) => {
         <body>
             <div class="box">
                 <h1>🚀 Jenkins Auto Deploy Working</h1>
-                <p>i want fix Test 3 it are not again check ╤φ+ΦÅ🔥</p>
+                <p>i want fix Test 3 it are not again check🔥</p>
                 <p>Status: <span style="color:yellow;">SUCCESS</span></p>
             </div>
         </body>
