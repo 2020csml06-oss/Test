6f9245643805fc2644e1c511b8d02741e7b7b032
P Pavan Kalyan <ppavankalyan79@gmail.com>
2026-04-19 16:01:53 +0530

    webhook auto build

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.end('Auto build working 🔥');
+    res.end('Auto webhook to ngrok build working 🔥');
 });
 
 server.listen(3000, () => {
